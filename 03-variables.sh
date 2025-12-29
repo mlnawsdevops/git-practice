1a4de5c62be546a01ae0506ed582e2a9aa576bf0
mlnawsdevops <mlnpublicact365@gmail.com>
2025-12-29 16:31:33 +0530

    shell

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,10 +3,10 @@
 PERSON1 = Ramesh
 PERSON2 = Suresh
 
-echo "${PERSON1}:: Hi ${PERSON2}, How are you?"
+echo "$PERSON1:: Hi $PERSON2, How are you?"
 
-echo "${PERSON2}:: Hello ${PERSON1}. Iam fine. How are you doing?"
+echo "$PERSON2:: Hello $PERSON1. Iam fine. How are you doing?"
 
-echo "${PERSON1}:: Iam doing good ${PERSON2}. what's going on?"
+echo "$PERSON1:: Iam doing good $PERSON2. what's going on?"
 
-echo "${PERSON2}:: I started learning shell script ${PERSON1}"
+echo "$PERSON2:: I started learning shell script $PERSON1"
