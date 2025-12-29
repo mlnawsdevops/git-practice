9df4ab0846bf70488af620bf9b44b6c9a14d27c9
mlnawsdevops <mlnpublicact365@gmail.com>
2025-12-29 16:25:10 +0530

    shell

--- 02-conservation.sh
+++ 02-conservation.sh
@@ -3,3 +3,7 @@
 echo "Ramesh:: Hi Suresh, How are you?"
 
 echo "Suresh:: Hello Ramesh. Iam fine. How are you doing?"
+
+echo "Ramesh:: Iam doing good suresh. what's going on?"
+
+echo "Suresh:: I started learning shell script Ramesh"
