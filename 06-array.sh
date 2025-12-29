22a2bdc5ce88148ab2a98bcb81f92181adcc2ce0
mlnawsdevops <mlnpublicact365@gmail.com>
2025-12-29 17:07:33 +0530

    shell

---
+++ 06-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FRUITS=("APPLE" "KIWI" "ORANGE")
+
+echo "First Fruit is: ${FRUITS[0]}"
+echo "Second Fruit is: ${FRUITS[1]}"
+echo "Third Fruit is: ${FRUITS[2]}"
+
+echo "First Fruit is: ${FRUITS[@]}"
