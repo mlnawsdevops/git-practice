2e3b23ac386cfd642ccbdbc5b503d5acda02c0d9
mlnawsdevops <mlnpublicact365@gmail.com>
2025-12-30 21:37:14 +0530

    shell

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#/var/log/shell-script/16-redirectors-<timestamp>.log
+set -e
 
 LOGS_FOLDER="/var/log/shell-script"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+SCRIPT_NAME=$(basename "$0" .sh)
 TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
-
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 
 mkdir -p $LOGS_FOLDER
@@ -14,18 +13,18 @@ USERID=$(id -u)
 
 R="\e[31m"
 G="\e[32m"
-N="\e[0m"
 Y="\e[33m"
+N="\e[0m"
 
-CHECK_ROOT(){
+CHECK_ROOT() {
     if [ $USERID -ne 0 ]
     then
-        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
+        echo -e "$R Please run this script with root privileges $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
 
-VALIDATE(){
+VALIDATE() {
     if [ $1 -ne 0 ]
     then
         echo -e "$2 is..$R Failed $N" | tee -a $LOG_FILE
@@ -35,7 +34,7 @@ VALIDATE(){
     fi
 }
 
-USAGE(){
+USAGE() {
     echo -e "$R USAGE:$N sudo sh 16-redirectors.sh package1 package2"
     exit 1
 }
@@ -44,20 +43,18 @@ echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 CHECK_ROOT
 
-if [ $# -eq 0 ] #sudo sh 16-redirectors.sh 
+if [ $# -eq 0 ]
 then
     USAGE
 fi
 
-
-for package in "$@" 
-
+for package in "$@"
 do
-    dnf list installed $package | tee -a $LOG_FILE
+    dnf list installed $package &>> $LOG_FILE
     if [ $? -ne 0 ]
-    then 
+    then
         echo -e "$package is $R not installed, going to install it $N" | tee -a $LOG_FILE
-        dnf install $package -y &>>LOG_FILE
+        dnf install $package -y &>> $LOG_FILE
         VALIDATE $? "Installing $package"
     else
         echo -e "$package is already $Y installed, nothing to do...$N" | tee -a $LOG_FILE
