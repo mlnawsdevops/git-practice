f2401d596adc814aa0e7a60e90fd9f606ab0b4c2
mlnawsdevops <mlnpublicact365@gmail.com>
2025-12-30 21:46:41 +0530

    shell

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -1,41 +1,37 @@
 #!/bin/bash
 
-set -e
-
 LOGS_FOLDER="/var/log/shell-script"
-SCRIPT_NAME=$(basename "$0" .sh)
-TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
-
 mkdir -p $LOGS_FOLDER
 
 USERID=$(id -u)
-
 R="\e[31m"
 G="\e[32m"
-Y="\e[33m"
 N="\e[0m"
+Y="\e[33m"
 
-CHECK_ROOT() {
+CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
-        echo -e "$R Please run this script with root privileges $N" | tee -a $LOG_FILE
+        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
 
-VALIDATE() {
+VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is..$R Failed $N" | tee -a $LOG_FILE
+        echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "$2 is..$G Success $N" | tee -a $LOG_FILE
+        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
-USAGE() {
-    echo -e "$R USAGE:$N sudo sh 16-redirectors.sh package1 package2"
+USAGE(){
+    echo -e "$R USAGE:: $N sudo sh 16-redirectors.sh package1 package2 ..."
     exit 1
 }
 
@@ -48,15 +44,15 @@ then
     USAGE
 fi
 
-for package in "$@"
+for package in $@ # $@ refers to all arguments passed to it
 do
-    dnf list installed $package | tee -a $LOG_FILE
+    dnf list installed $package &>>$LOG_FILE
     if [ $? -ne 0 ]
     then
-        echo -e "$package is $R not installed, going to install it $N" | tee -a $LOG_FILE
-        dnf install $package -y | tee -a $LOG_FILE
+        echo "$package is not installed, going to install it.." | tee -a $LOG_FILE
+        dnf install $package -y &>>$LOG_FILE
         VALIDATE $? "Installing $package"
     else
-        echo -e "$package is already $Y installed, nothing to do...$N" | tee -a $LOG_FILE
+        echo -e "$package is already $Y installed..nothing to do $N" | tee -a $LOG_FILE
     fi
 done
