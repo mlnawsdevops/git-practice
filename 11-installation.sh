01db2d3acca2e3ec1067c8861b09ca069ef6abb6
mlnawsdevops <mlnpublicact365@gmail.com>
2025-12-30 11:23:22 +0530

    commands

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+# dnf install git -y
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Please run this script with root priveleges"
+    exit 1
+fi 
+
 dnf install git -y
+
