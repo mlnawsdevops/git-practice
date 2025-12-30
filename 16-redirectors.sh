d131259eb42931099ba457460d9e34680f7794f0
mlnawsdevops <mlnpublicact365@gmail.com>
2025-12-30 22:09:09 +0530

    shell

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -20,7 +20,7 @@ Y="\e[33m"
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
-        echo -e "$R Please run this script with root priveleges $N" &>>$LOG_FILE
+        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
