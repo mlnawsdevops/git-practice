0c0884d044cceddb56ccc3f0916047250a933051
mlnawsdevops <mlnpublicact365@gmail.com>
2025-12-30 12:56:57 +0530

    shell

---
+++ 13-colors.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0]
+    then
+        echo -e "$R Please run this script with root priveleges $N"
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 is...$R Failed $N"
+    else
+        echo -e "$2 is...$G Success $N"
+    fi
+}
+
+CHECK_ROOT
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then   
+    echo -e "$R Git is not installed, going to install it... $N"
+    dnf install git -y
+    VALIDATE $? "Installing Git"
+else
+    echo -e "$G Git is already installed, nothing to do...$N"
+fi
+
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo -e "$R Mysql is not installed, going to install it $N"
+    dnf install mysql -y
+    VALIDATE $? "Installing MYSQL"
+else
+    echo -e "$G MYSQL is already installed, nothing to do...$N"
+fi
