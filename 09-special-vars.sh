90deeb9fe6064d62ce886a5621fe260329a43b5a
mlnawsdevops <mlnpublicact365@gmail.com>
2025-12-30 09:16:42 +0530

    commands

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 echo "All variables passed to the script: $@"
-echo "number of variables passed: $#"
+echo "Number of variables passed: $#"
+echo "Script name: $0"
+echo "Current working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "PID of the script executing now: $$"
+echo "PID of last background command: $!"
