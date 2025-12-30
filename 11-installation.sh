83ebb1d3ddcade7772daed989c43aac22c208e16
mlnawsdevops <mlnpublicact365@gmail.com>
2025-12-30 11:46:00 +0530

    shell

--- 11-installation.sh
+++ 11-installation.sh
@@ -12,12 +12,19 @@ fi
 
 dnf install git -y
 
-dnf list installed git 
+dnf list installed gittt
 
 if [ $? -ne 0 ]
 then 
     echo "Git is not installed, going to install it..."
     dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "Git installation is not success...check it"
+        exit 1
+    else
+        echo "Git installation is successful"
+    fi
 else    
     echo "Git is already installed, nothing to do..."
 fi
