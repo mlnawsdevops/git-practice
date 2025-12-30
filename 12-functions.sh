15733f64cf28d3c9196663f9724982a0b37e91cb
mlnawsdevops <mlnpublicact365@gmail.com>
2025-12-30 12:32:48 +0530

    shell

---
+++ 12-functions.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+USERID=$(user -d)
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "Please run this script with root priveleges"
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2 is...Failed"
+    else
+        echo "$2 is...Sucess"
+}
+
+CHECK_ROOT
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then 
+    echo "Git is not installed...going to install it..."
+    dnf install git -y
+    VALIDATE $? "Installing Git..."
+else
+    echo "Git is already installed, nothinf to do..."
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then 
+    echo "Mysql is not installed...going to install"
+    dnf install mysql -y
+    VALIDATE $? "Installing Mysql"
+else
+    echo "Mysql is already installed, nothing to do..."
+fi
