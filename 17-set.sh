90e57386949f4531bbc39b8efb54b8120ff30f70
mlnawsdevops <mlnpublicact365@gmail.com>
2025-12-31 14:25:28 +0530

    git-practice

--- 17-set.sh
+++ 17-set.sh
@@ -2,6 +2,12 @@
 
 set -ex
 
+function(){
+    echo "failed at $1:$2"
+}
+
+trap 'function "${LINENO}" "${BASH_COMMAND}"' ERR #ERR is for error signal
+
 echo "Hello world success"
 echhhh "Hello world failure"
 echo "Hello world after failure"
